3798a82f50051ddc43a8991fa9d2b5a2cd002b96
Matěj Vopálka <matej.vopalka@gmail.com>
2020-06-26 12:33:27 +0200

    Classification chapter

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.datasets import load_boston
 
-presidents_df = pd.read_csv('https://sololearn.com/uploads/files/president_heights_party.csv', index_col='name')
+boston_dataset = load_boston()
+boston = pd.DataFrame(boston_dataset.data, columns=boston_dataset.feature_names)
+boston['MEDV'] = boston_dataset.target
+print(boston[['CHAS', 'RM', 'AGE', 'RAD', 'MEDV']].head())
+print()
+print(boston.describe().round(2))
 
-plt.scatter(presidents_df['height'], presidents_df['age'],
-            marker='_',
-            color='m')
-plt.xlabel('height')
-plt.ylabel('age')
-plt.title('U.S. presidents')
-plt.show()
+#print(boston_dataset.target)
+#boston['MEDV'] = boston_dataset.target
+#print(boston.shape)
